c22015519d03f811015112c7e4d2ac33566497e8
Shrushti-Nagpure <144778839+Shrushti-Nagpure@users.noreply.github.com>
2023-09-26 12:58:11 +0530

    Add files via upload

---
+++ simple_interest.py
@@ -0,0 +1,10 @@
+def s_i():
+    p = float(input("Enter the principal Amount"))
+    r = float(input("Enter the Interest"))
+    t = float(input("Enter the period "))
+    sii = (p*r*t)/100
+    print("The simple interest is ",sii)
+    return sii
+s_i()
+
+        
